41dbe72540312656a996f136cd89f8b86d13a3cd
Amira Walid <amirawaleed012@gmail.com>
2023-10-09 23:56:35 +0300

    hidden discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def print_names():
+    for name in sorted(dir(hidden_4)):
+        if name[:2] != '__':
+            print(name)
+
+
+if __name__ == "__main__":
+    print_names()
