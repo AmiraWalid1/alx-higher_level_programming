c379959fc8f07cc198c09448b4d56b0b3bcb728d
Amira Walid <amirawaleed012@gmail.com>
2023-10-09 17:50:15 +0300

    mandatory

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper_str = ''
+    for ch in str:
+        if ord('z') >= ord(ch) >= ord('a'):
+            upper_str += chr(ord(ch)-32)
+        else:
+            upper_str += ch
+    print(upper_str)
