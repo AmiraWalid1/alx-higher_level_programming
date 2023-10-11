7c402d268f6178346b9cf5c73c2dc8351b75b097
Amira Walid <amirawaleed012@gmail.com>
2023-10-11 22:57:28 +0300

    mandatory

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+def no_c(my_string):
+    new_str = ''
+    for ch in my_string:
+        if ch not in ['c', 'C']:
+            new_str += ch
+    return new_str
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for lst in matrix:
+        for item in lst:
+            print("{:d}".format(item), end=(" " if l[-1] != item else "\n"))
