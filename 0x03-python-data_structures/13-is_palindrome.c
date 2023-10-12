c1f2afef1323a9838eb2a2f3b11b901b6e5cba2a
Amira Walid <amirawaleed012@gmail.com>
2023-10-13 01:15:49 +0300

    fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -14,7 +14,7 @@ listint_t *addNodeToBegin(listint_t **head, int n)
 	if (!head)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
-	if (new_node)
+	if (!new_node)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = *head;
