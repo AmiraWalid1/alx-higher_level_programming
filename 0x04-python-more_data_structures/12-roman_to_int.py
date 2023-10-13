49b1021b39f502671635fa9f7379f15d467fe7c1
Amira Walid <amirawaleed012@gmail.com>
2023-10-13 23:19:06 +0300

    fix issuse

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,9 @@ def roman_to_int(roman_string):
     my_dir = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     roman_string = list(roman_string)
-    for i in range(len(roman_string) -1 , -1, -1): 
-        if i + 1 < len(roman_string) and (my_dir[roman_string[i + 1]] > my_dir[roman_string[i]]):
+    for i in range(len(roman_string) - 1, -1, -1):
+        if i + 1 < len(roman_string) and \
+         (my_dir[roman_string[i + 1]] > my_dir[roman_string[i]]):
             num -= my_dir[roman_string[i]]
         else:
             num += my_dir[roman_string[i]]
