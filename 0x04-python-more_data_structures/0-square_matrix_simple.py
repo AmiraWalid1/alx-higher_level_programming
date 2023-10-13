043bc9c9ea54b3a97332ddbf5a95577336afba96
Amira Walid <amirawaleed012@gmail.com>
2023-10-13 15:28:24 +0300

    mandatory

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     square_matrix = []
-    for lst in matrix:
-        square_lst = []
-        for item in lst:
-            square_lst.append(item * item)
-        square_matrix.append(square_lst)
+    for row in matrix:
+        square_row = [item ** 2 for item in row]
+        square_matrix.append(square_row)
     return square_matrix
