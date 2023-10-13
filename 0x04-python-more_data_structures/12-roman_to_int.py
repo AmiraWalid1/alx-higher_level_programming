04b950f76f2c930fcc17e873626f9161ccd8298f
Amira Walid <amirawaleed012@gmail.com>
2023-10-13 23:24:54 +0300

    fix pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,8 @@ def roman_to_int(roman_string):
     num = 0
     roman_string = list(roman_string)
     for i in range(len(roman_string) - 1, -1, -1):
-        if i + 1 < len(roman_string) and (my_dir[roman_string[i + 1]] > my_dir[roman_string[i]]):
+        if i + 1 < len(roman_string) and \
+           (my_dir[roman_string[i + 1]] > my_dir[roman_string[i]]):
             num -= my_dir[roman_string[i]]
         else:
             num += my_dir[roman_string[i]]
