28307a71052de4307f4082abf6738bea87a2a535
Amira Walid <amirawaleed012@gmail.com>
2023-10-13 22:39:36 +0300

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or isinstance(roman_string, str):
+        return 0
+    my_dir = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    for ch in list(roman_string):
+        num += my_dir[ch]
+    return num
