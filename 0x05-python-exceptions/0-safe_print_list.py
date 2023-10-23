f17bf01f95328ecd9c8c3a9fc72d99baf56d03d1
Amira Walid <amirawaleed012@gmail.com>
2023-10-23 19:07:52 +0300

    fix issuse

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if x <= 0:
+        print("")
+        return 0
     for idx in range(x):
         try:
             print(f"{my_list[idx]}", end="")
