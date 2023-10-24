0291a46f6adc8e949a14f8b63479b66a94a3ba47
Amira Walid <amirawaleed012@gmail.com>
2023-10-25 00:38:24 +0300

    mandatory

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,12 +5,7 @@
 class Square:
     """Square class defined"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     @property
     def size(self):
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,12 +5,7 @@
 class Square:
     """Square class defined"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     @property
     def size(self):
@@ -33,6 +28,4 @@ class Square:
             print('')
             return
         for i in range(self.__size):
-            for j in range(self.__size):
-                print('#', end='')
-            print('')
+            print("#" * self.__size)
