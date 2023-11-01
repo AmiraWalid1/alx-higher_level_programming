fc058bf858e05afa2b945176a79b6e1399fc5f5f
Amira Walid <amirawaleed012@gmail.com>
2023-11-01 17:04:57 +0200

    adv

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,6 +0,0 @@
-def increment(n):
-    n = n + [4]
-
-l = [1, 2, 3]
-increment(l)
-print(l)
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """moudle contain LockedClass Class"""
+
+
 class LockedClass:
     """define class"""
     __slots__ = ['first_name']
