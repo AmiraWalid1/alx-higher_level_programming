1ac9b2adec37e6d53e537297b2885b978df03807
Amira Walid <amirawaleed012@gmail.com>
2024-01-10 22:52:29 +0200

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+''' module contain to_json_string function '''
+
+
+def to_json_string(my_obj):
+    '''
+    function that returns the JSON representation of an object (string)
+    '''
+    return json.dumps(my_obj)
