52d5be21927df7c4f95f7cebed73a5e6769ce8c2
Amira Walid <amirawaleed012@gmail.com>
2024-01-19 20:32:59 +0200

    Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' This Module contain pascal_triangle function'''
+
+
+def pascal_triangle(n):
+    '''
+    function that returns a list of lists of integers representing
+    the Pascal’s triangle of n
+    '''
+    My_list = []
+    for i in range(5):
+        # First element = 1
+        sublist = [1]
+
+        for j in range(1, i):
+            sublist.append(My_list[i-1][j] + My_list[i-1][j-1])
+
+        # Last element = 1
+        if i != 0:
+            sublist.append(1)
+
+        My_list.append(sublist)
+
+    return My_list
