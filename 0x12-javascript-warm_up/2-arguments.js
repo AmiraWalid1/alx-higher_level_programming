84e574c6a96d8dcf50bacdf5f98b3c324408293d
Amira Walid <amirawaleed012@gmail.com>
2024-03-12 22:38:30 +0200

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+let sz = process.argv.length;
+let message;
+if (sz == 2)
+{
+    message = 'No argument';
+}
+else if (sz == 3)
+{
+    message = 'Argument found';
+}
+else
+{
+    message = 'Arguments found';
+}
+console.log(message);
