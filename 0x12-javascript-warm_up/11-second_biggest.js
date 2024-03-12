cdf2578ec05ab975b0e18ec1cfc55bc67e899b36
Amira Walid <amirawaleed012@gmail.com>
2024-03-13 00:19:18 +0200

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const arr = process.argv.slice(2);
-arr.sort();
-arr.reverse();
 if (arr.length < 2) {
   console.log(0);
 } else {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = parseInt(arr[i]);
+  }
+  arr.sort((a, b) => b - a);
   console.log(arr[1]);
 }
