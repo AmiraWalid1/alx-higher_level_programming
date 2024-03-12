bc8f9b022c3afd3b411f15521527b36c4cab335d
Amira Walid <amirawaleed012@gmail.com>
2024-03-12 22:46:05 +0200

    Arguments fixes semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-let sz = process.argv.length;
+const sz = process.argv.length;
 let message;
-if (sz == 2)
-{
-    message = 'No argument';
-}
-else if (sz == 3)
-{
-    message = 'Argument found';
-}
-else
-{
-    message = 'Arguments found';
+if (sz === 2) {
+  message = 'No argument';
+} else if (sz === 3) {
+  message = 'Argument found';
+} else {
+  message = 'Arguments found';
 }
 console.log(message);
