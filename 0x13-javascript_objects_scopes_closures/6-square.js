ef61c197fc39e2f997a6cf9326cdc8caf2d62d7e
Amira Walid <amirawaleed012@gmail.com>
2024-03-15 02:18:58 +0200

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      let shape = '';
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          shape += c;
+        }
+        if (i !== this.height - 1) {
+          shape += '\n';
+        }
+      }
+      console.log(shape);
+    }
+  }
+}
+module.exports = Square;
