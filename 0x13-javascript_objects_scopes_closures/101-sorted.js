69456bfed711c142efa4b785d1bbc502c61be440
Amira Walid <amirawaleed012@gmail.com>
2024-03-15 22:35:36 +0200

    Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 const newDict = {};
-for (const property of Object.entries(dict)) {
-  newDict[property[1]] = [];
-}
 for (const [key, value] of Object.entries(dict)) {
-  newDict[value] = newDict[value].concat(key);
+  newDict[value] = (newDict[value] === undefined ? [key] : newDict[value].concat(key));
 }
 console.log(newDict);
