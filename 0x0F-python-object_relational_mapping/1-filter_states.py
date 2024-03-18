51437d6d7cda73e9da1a358053311961e9f126a9
Amira Walid <amirawaleed012@gmail.com>
2024-03-19 01:56:49 +0200

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' script that lists all states from the database hbtn_0e_0_usa '''
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mydb = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("""
+                    SELECT * FROM states
+                    WHERE name REGEXP '^N.*'
+                    ORDER BY states.id
+                    """)
+    states = mycursor.fetchall()
+    for record in states:
+        print(record)
+    mycursor.close()
+    mydb.close()
