402e73088501c3ddaed58069dd15d59b5e115b27
Amira Walid <amirawaleed012@gmail.com>
2024-03-21 16:11:08 +0200

    15. City relationship documentation

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""class definition of a State"""
+""" script that creates the State “California” with the City “San Francisco”
+from the database hbtn_0e_100_usa """
 
 from sys import argv
 from sqlalchemy import create_engine
