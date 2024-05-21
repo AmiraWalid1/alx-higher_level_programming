c647cdae05df8d1f81cabd9426f0e072d66ffe12
Amira Walid <amirawaleed012@gmail.com>
2024-05-22 02:29:57 +0300

    3. Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const id = process.argv[2];
+
+request.get(url + id, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
