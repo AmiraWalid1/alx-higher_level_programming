e15ec47622cc0ec33661ed1f96269309091d668f
Amira Walid <amirawaleed012@gmail.com>
2024-05-22 02:11:32 +0300

    2. Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('code: ' + res.statusCode);
+});
