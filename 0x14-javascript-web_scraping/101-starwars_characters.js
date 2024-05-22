afd9628ed080b22c5c5718c0ac49eecdbeb378c8
Amira Walid <amirawaleed012@gmail.com>
2024-05-22 03:45:37 +0300

    8. Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const id = process.argv[2];
+
+request.get(url + id, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const film = JSON.parse(body);
+    for (const character of film.characters) {
+      request(character, (err, res, body) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
