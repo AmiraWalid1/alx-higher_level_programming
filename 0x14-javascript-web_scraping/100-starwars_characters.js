ad700422b7fae088d98c074e5f923bf3129fda09
Amira Walid <amirawaleed012@gmail.com>
2024-05-22 03:43:03 +0300

    7. Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,19 +3,19 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 const id = process.argv[2];
 
-request.get(url+id, (err, res, body) => {
+request.get(url + id, (err, res, body) => {
   if (err) {
     console.log(err);
   } else {
-    const film = JSON.parse(body)
-    for (let character of film.characters) {
-        request(character, (err, res, body) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(JSON.parse(body).name)
-            }
-        })
+    const film = JSON.parse(body);
+    for (const character of film.characters) {
+      request(character, (err, res, body) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
     }
   }
 });
