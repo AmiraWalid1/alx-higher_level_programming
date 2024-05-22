9c4045ed505273f9790cdddecb45412ab616f470
Amira Walid <amirawaleed012@gmail.com>
2024-05-22 15:08:58 +0300

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-let wedgeAntilles = '';
-for (const i in url) {
-  if (url.substr(i, 4) === '/api') {
-    wedgeAntilles += url.substr(i, 4);
-    break;
-  } else {
-    wedgeAntilles += url[i];
-  }
-}
 
 request.get(url, (err, res, body) => {
   if (err) {
@@ -18,7 +9,7 @@ request.get(url, (err, res, body) => {
     let cnt = 0;
     const films = JSON.parse(body).results;
     for (const film of films) {
-      if (film.characters.includes(wedgeAntilles + '/people/18/')) {
+      if (film.characters.find(character => character.endsWith('/18/'))) {
         cnt++;
       }
     }
